9176ae2821a816013454ea0ce5090565199e63ff
jagnat <jagnat4000@gmail.com>
2021-04-10 14:12:28 -0700

    Add player joining/leaving and beginning of playerstate

--- bababooi-backend/server.py
+++ bababooi-backend/server.py
@@ -1,14 +1,42 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+import json
+import gamestate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, logger=True, engineio_logger=True, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on('test_event')
-def handle_message(data):
-	print(data)
-	send("ack")
+def broadcast_players(room):
+    players = gamestate.get_players(room)
+    msg = {}
+    msg['room'] = room
+    msg['players'] = players
+    msg_str = json.dumps(msg)
+    emit('players', msg_str, to=room)
+
+@socketio.on('join_game')
+def join_game(data):
+    join_packet = json.loads(data)
+    player_exists = gamestate.add_player(join_packet)
+    if player_exists:
+        emit('error', 'Player already exists!')
+        return
+    join_room(join_packet['room'])
+    broadcast_players(join_packet['room'])
+
+@socketio.on('leave_game')
+def leave_game(data):
+    leave_packet = json.loads(data)
+    game_still_exists = gamestate.remove_player(leave_packet)
+    leave_room(leave_packet['room'])
+    print("Game_still_exist:", game_still_exists)
+    if game_still_exists:
+        broadcast_players(leave_packet['room'])
+
+@socketio.on('start_game')
+def start_game(data):
+    pass
 
 if __name__ == '__main__':
     socketio.run(app)
