14b6d232093e8198c0b7e1a0bbba1382a6ab9670
Nick <n.monkey.d@gmail.com>
2021-04-10 20:49:07 -0700

    rewrite JoinGame for modified architecture

--- bababooi-infra/lambda-source/JoinGame.py
+++ bababooi-infra/lambda-source/JoinGame.py
@@ -1,44 +1,89 @@
-import json, re, os
+import json, re, os, random
 import boto3
 from boto3.dynamodb.conditions import Key
 import logging
 
+logger = logging.getLogger()
+
 id_match = re.compile("[0-9A-Z]{6}")
+ddb = boto3.resource('dynamodb')
 
-def lambda_handler(event, context):
+def find_host(userId):
+    table = ddb.Table(os.environ['DDB_HOST_TABLE'])
 
-    ddb = boto3.resource('dynamodb')
-    session_table = ddb.Table(os.environ['DDB_GAME_SESSION_TABLE'])
-    
-    gameId = event['gameId']
-    logging.info(f'parsed game ID {gameId}')
+    scan_kwargs = {
+        'ProjectionExpression':'hostname'
+    }
 
-    if not id_match.match(gameId):
-        return {
-            'statusCode': 400,
-            'error': 'InvalidGameId'
-        }
+    response = table.scan(**scan_kwargs)
+    servers = response['Items']
+    random.shuffle(servers)
 
-    
+    logger.debug(servers)
+
+    # query each server to find availability, for testing we're just grabbing the first one
+    # eventually we'll need to handle 503-type scenarios
+
+    return(servers[0]['hostname'])
+
+def join_session(userId, gameId):
+
     # query DDB to find game session
+    session_table = ddb.Table(os.environ['DDB_GAME_SESSION_TABLE'])
 
     get_session_resp = session_table.get_item(
             Key={'GameSessionId':gameId}
         )
-    if 'Item' not in get_session_resp:
-        return {
-            'statusCode': 404,
-            'error': 'SessionNotFound'
-        }
-    
-    
-    session_hostname = get_session_resp['Item']['hostname']
 
-    resp = {
-        'host':session_hostname
-    }
+    if 'Item' not in get_session_resp:
+        return None
+    else:
+        return get_session_resp['Item']['hostname']
+
+def lambda_handler(event, context):
+    print(event)
+    userId = event['userId']
+
+    session_hostname = ''
     
+    # No gameId means we're requesting a new session
+    if 'gameId' not in event or event['gameId'] == '':
+
+        # check with game servers to find a host
+        session_hostname = find_host(userId)
+        if not session_hostname:
+            return {
+                'statusCode': 503,
+                'error':'NoHostFound'
+            }
+
+    # gameId present, let's try to find the hostname for that session
+    else:
+        gameId = event['gameId']
+
+        logging.info(f'parsed game ID {gameId}')
+
+        # validate that it's a good gameId
+        if not id_match.match(gameId):
+            return {
+                'statusCode': 400,
+                'error': 'InvalidGameId'
+            }
+
+        session_hostname = join_session(userId, gameId)
+
+        if not session_hostname:
+            return {
+                'statusCode': 404,
+                'error': 'GameIdNotFound'
+            }
+
+
+    # If we got this far, we have a hostname
     return {
         'statusCode': 200,
-        'body': resp
+        'host':session_hostname
     }
+
+
+print(lambda_handler({'userId':'test','gameId':'YD9C6G'}, None))
