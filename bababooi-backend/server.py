413fe94397c77ef7c657f9408a43d7ac2c0534fc
jagnat <jagnat4000@gmail.com>
2021-04-11 00:44:10 -0700

    Add local cache of resources

--- bababooi-backend/server.py
+++ bababooi-backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_cors import CORS
-import json, io, random, string, boto3
+import json, io, random, string, boto3, os
 from PIL import Image
 import gamestate
 
@@ -81,16 +81,24 @@ def preload():
     bucket = s3.Bucket('bababooi')
 
     # Load bababooi data
-    info = bucket.Object('games/adv_draw/info.json')
-    gamestate.bababooi_data['info'] = json.loads(info.get()['Body'].read())
-    gamestate.bababooi_data['img'] = {}
-    for class_name in gamestate.bababooi_data['info']['class_names']:
-        filename = 'games/adv_draw/' + class_name + '.ndjson'
-        imgs = bucket.Object(filename)
-        gamestate.bababooi_data['img'][class_name] = []
-        imgFileStr = imgs.get()['Body'].read()
-        for line in imgFileStr.splitlines():
-            gamestate.bababooi_data['img'][class_name].append(json.loads(line))
+
+    # TODO: Remove this cache for prod
+    if not os.path.isfile('cached_bababooi.txt'):
+        info = bucket.Object('games/adv_draw/info.json')
+        gamestate.bababooi_data['info'] = json.loads(info.get()['Body'].read())
+        gamestate.bababooi_data['img'] = {}
+        for class_name in gamestate.bababooi_data['info']['class_names']:
+            filename = 'games/adv_draw/' + class_name + '.ndjson'
+            imgs = bucket.Object(filename)
+            gamestate.bababooi_data['img'][class_name] = []
+            imgFileStr = imgs.get()['Body'].read()
+            for line in imgFileStr.splitlines():
+                gamestate.bababooi_data['img'][class_name].append(json.loads(line))
+        with open('cached_bababooi.txt', 'w') as fp:
+            fp.write(json.dumps(gamestate.bababooi_data))
+    else:
+        with open('cached_bababooi.txt', 'r') as fp:
+            gamestate.bababooi_data = json.loads(fp.read())
     # print(gamestate.bababooi_data['info'])
     # print(gamestate.bababooi_data['img']['tennis_racquet'])
 
