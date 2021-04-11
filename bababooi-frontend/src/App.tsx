f41680c5b82e29490bf0d959ce5f6b7181817a94
Christian Brintnall <christianbrintnall@gmail.com>
2021-04-11 03:37:59 -0700

    stuff

--- bababooi-frontend/src/App.tsx
+++ bababooi-frontend/src/App.tsx
@@ -1,76 +1,139 @@
 import "./App.css";
-import 'aesthetic-css/aesthetic.css';
+import "aesthetic-css/aesthetic.css";
 
-import React, { useEffect, useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { LandingPage } from './pages/Landing';
-import { NewGame } from './pages/NewGame';
-import { io } from 'socket.io-client';
-import cfg from "./config";
-import { errorSubject, ErrorEvent } from "./events";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { LandingPage } from "./pages/Landing";
+import { NewGame } from "./pages/NewGame";
+import { LoginScreen } from "./pages/LoginScreen";
+import { errorSubject, ErrorEvent, newUserSubject } from "./events";
+import { DrawingGame } from "./pages/Drawing";
+
+type User = {
+  username: string;
+};
+
+export const UserContext = React.createContext<User | undefined>(undefined);
+
+export const getUsername = (): string | undefined => {
+  return localStorage.getItem('USERNAME') || undefined;
+}
 
 function NotificationsHost() {
-  const [ error, setError ] = useState<ErrorEvent>();
+  const [error, setError] = useState<ErrorEvent>();
 
   useEffect(() => {
-    errorSubject.subscribe(errorEvent => 
-      setError(errorEvent)
-    )
-  }, [])
+    errorSubject.subscribe((errorEvent) => setError(errorEvent));
+  }, []);
 
   return (
-    <div style={{ position: 'fixed', left: '30px', bottom: '30px' }}>
-      {
-        error && 
+    <div style={{ position: "fixed", left: "30px", bottom: "30px" }}>
+      {error && (
         <div className="aesthetic-notification is-active">
-          <button 
-            className="dismiss" 
-            onClick={() => setError(undefined)}
-          >
+          <button className="dismiss" onClick={() => setError(undefined)}>
             X
           </button>
-          <div 
-            style={{ display: 'flex', flexDirection: 'row', paddingRight: '32px' }} 
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              paddingRight: "32px",
+            }}
             className="aesthetic-notification-content aesthetic-pepsi-red-color"
           >
             <div>
               <div className="aesthetic-effect-crt">
-                <div style={{
-                  height: '50px',
-                  width: '50px',
-                  background: 'url("/error.png")',
-                  backgroundSize: 'contain'
-                }}>
-                </div>
+                <div
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    background: 'url("/error.png")',
+                    backgroundSize: "contain",
+                  }}
+                ></div>
               </div>
-              <span style={{verticalAlign: "100%"}}> { `ERROR: ${error.userMessage}` } </span>
+              <span style={{ verticalAlign: "100%" }}>
+                {" "}
+                {`ERROR: ${error.userMessage}`}{" "}
+              </span>
             </div>
           </div>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 function App() {
+  const [ user, setUser ] = useState<User|undefined>(undefined);
+
+  useEffect(() => {
+    if (localStorage) {
+      // const user = localStorage.getItem('USERNAME')
+
+      // if (user) {
+      //   setUser({ username: user })
+      // }
+
+      newUserSubject.subscribe((user) => {
+        localStorage.setItem('USERNAME', user.username);
+        // setUser({ username: user.username })
+      });
+    }
+  }, [])
+
   return (
     <Router>
       <div id="app">
         <NotificationsHost />
         <Switch>
-          <Route
-            path="/game"
-            render={routeProps => <NewGame {...routeProps} />}
-          />
-          <Route 
-            path="/" 
-            render={routeProps => <LandingPage {...routeProps} />} 
-          />
+          <UserContext.Provider value={user}>
+            <Route
+              exact
+              path="/login"
+              render={(routeProps) => { 
+                if (getUsername()) {
+                  return <Redirect to="/game" />
+                }
+              
+                return <LoginScreen {...routeProps} />
+              }}
+            />
+            <Route
+              exact
+              path="/drawing/:gameId"
+              render={(routeProps) => {
+               if (!getUsername()) {
+                 console.log(getUsername())
+                 return <Redirect to="/login" />
+               }
+               
+               return <DrawingGame {...routeProps} />
+              }}
+            />
+            <Route
+              exact
+              path="/game"
+              render={(routeProps) => { 
+                if (!getUsername()) {
+                  return <Redirect to="/login" />
+                }
+
+                return <NewGame {...routeProps} /> 
+              }}
+            />
+            <Route
+              exact
+              path="/"
+              render={(routeProps) => { 
+                if (!getUsername()) {
+                  return <Redirect to="/login" />
+                }
+
+                return <LandingPage {...routeProps} /> 
+              }}
+            />
+          </UserContext.Provider>
         </Switch>
       </div>
     </Router>
---
+++ bababooi-frontend/src/pages/LoginScreen.tsx
@@ -0,0 +1,64 @@
+import React, { CSSProperties } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { newUserSubject } from "../events";
+
+const style: CSSProperties = {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+type LoginState = {
+  login: string;
+};
+
+export class LoginScreen extends React.Component<
+  RouteComponentProps,
+  LoginState
+> {
+  constructor(props: RouteComponentProps) {
+    super(props);
+
+    this.state = {
+      login: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="aesthetic-blue-bg-color" style={style}>
+        <div
+          style={{ margin: "12px" }}
+          className="aesthetic-windows-95-container"
+        >
+          <span> LOGIN: </span>
+          <hr />
+          <div>
+            <input
+              className="aesthetic-windows-95-text-input"
+              type="text"
+              value={this.state.login}
+              onChange={(e) => this.setState({ login: e.target.value })}
+            />
+            <div style={{marginTop: "6px" }} className="aesthetic-windows-95-button">
+              <button 
+                disabled={this.state.login.length===0}
+                onClick={() => {
+                  if (this.state.login.length > 0) {
+                    newUserSubject.next({ username: this.state.login });
+                    this.props.history.push('/game')
+                  }
+                }}
+              >
+                LOGIN
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
