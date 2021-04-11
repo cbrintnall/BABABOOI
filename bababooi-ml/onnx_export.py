1201ce4dd7c9af4affed22507edcdf66c8ee482f
Eric Slyman <pianoguy197@gmail.com>
2021-04-10 20:55:02 -0700

    Add quickdraw onnx export

--- bababooi-ml/onnx_export.py
+++ bababooi-ml/onnx_export.py
@@ -3,20 +3,23 @@ import argparse
 
 import torch
 
-from models import DummyModel
+from models import DummyModel, QuickDraw
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-c', '--checkpoint', help='Path to a model checkpoint.')
     parser.add_argument('-o', '--out', default='model.onnx', help='ONNX write path')
-    parser.add_argument('-t', '--type', choices=['dummy'], default='dummy', help='Type of model to export.')
+    parser.add_argument('-t', '--type', choices=['dummy', 'quickdraw'], default='dummy',
+                        help='Type of model to export.')
     return parser.parse_args()
 
 
 def main(args):
     if args.type == 'dummy':
         model = DummyModel.load_from_checkpoint(args.checkpoint)
+    elif args.type == 'quickdraw':
+        model = QuickDraw.load_from_checkpoint(args.checkpoint)
     else:
         raise ValueError(f'Model type {args.type} not supported.')
 
