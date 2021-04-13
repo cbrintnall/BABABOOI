6c1474af480dee5687c61152ffe89a6ce090185e
Christian Brintnall <christianbrintnall@gmail.com>
2021-04-12 17:16:07 -0700

    add dockerfile to game server / integrate with compose

--- bababooi-backend/src/server.py
+++ bababooi-backend/src/server.py
@@ -1,47 +1,20 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_cors import CORS
-import json, io, random, string, boto3, os
+from model_interactions import preload
+import json, io, random, string
 from PIL import Image
 import gamestate
-
-def preload():
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('bababooi')
-
-    # Load bababooi data
-
-    # TODO: Remove this cache for prod
-    if not os.path.isfile('cached_bababooi.txt'):
-        info = bucket.Object('games/adv_draw/info.json')
-        gamestate.bababooi_data['info'] = json.loads(info.get()['Body'].read())
-        gamestate.bababooi_data['img'] = {}
-        for class_name in gamestate.bababooi_data['info']['class_names']:
-            filename = 'games/adv_draw/' + class_name + '.ndjson'
-            imgs = bucket.Object(filename)
-            gamestate.bababooi_data['img'][class_name] = []
-            imgFileStr = imgs.get()['Body'].read()
-            for line in imgFileStr.splitlines():
-                gamestate.bababooi_data['img'][class_name].append(json.loads(line))
-        with open('cached_bababooi.txt', 'w') as fp:
-            fp.write(json.dumps(gamestate.bababooi_data))
-    else:
-        with open('cached_bababooi.txt', 'r') as fp:
-            gamestate.bababooi_data = json.loads(fp.read())
- 
-    gamestate.masked_feud_data['prompts'] = []
-    gamestate.masked_feud_data['prompts'].append('I love eating [MASK].')
-    gamestate.masked_feud_data['prompts'].append('The first thing I do after work is [MASK].')
-    gamestate.masked_feud_data['prompts'].append('Who ate my [MASK]?')
-    gamestate.masked_feud_data['prompts'].append('I prefer [MASK] over dogs.')
-    gamestate.masked_feud_data['prompts'].append('I no longer love [MASK].')
-    
 
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
-preload()
+
+try:
+    preload()
+except Exception as e:
+    print(f"Exception preloading: {e}, this won't stop app start")
 
 @app.route('/status', methods=['GET'])
 def status():
